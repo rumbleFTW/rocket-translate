3a29c48048094d05fb82318d43643336a105644f
rumbleFTW <007rajdeepghosh@gmail.com>
2023-12-22 03:33:46 +0530

    Added 'add' endpt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
 #[macro_use]
 extern crate rocket;
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+
+struct Response {
+    params: String,
+    response: String,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Ok {
+    message: &'static str,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, Rocket!"
+fn root() -> Json<Ok> {
+    let response: Ok = Ok {
+        message: "Hello, World!",
+    };
+    Json(response)
+}
+
+#[get("/add/<num1>/<num2>")]
+fn add(num1: &str, num2: &str) -> Json<Response> {
+    let n1: f64 = num1.parse().unwrap();
+    let n2: f64 = num1.parse().unwrap();
+    let response = Response {
+        params: format!("{} {}", &num1, &num2),
+        response: format!("{}", n1 + n2),
+    };
+    Json(response)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![root, add])
 }
