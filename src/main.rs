53675bc42d6e1116e8b6fd3fe7bebb832abc7bbc
rumbleFTW <007rajdeepghosh@gmail.com>
2023-12-23 23:10:49 +0530

    Added all endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,68 @@
 #[macro_use]
 extern crate rocket;
-use rocket::serde::json::Json;
-use rocket::serde::Serialize;
+use rocket::serde::{json::Json, Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
-
-struct Response {
-    params: String,
-    response: String,
+struct TranslateRequest<'r> {
+    source_lang: &'r str,
+    target_lang: &'r str,
+    text: &'r str,
 }
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-struct Ok {
-    message: &'static str,
+struct RootResponse<'r> {
+    version: &'r str,
+    status: &'r str,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct LanguageResponse<'r> {
+    languages: [&'r str; 10],
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct TranslateResponse<'r> {
+    success: bool,
+    source_language: &'r str,
+    target_language: &'r str,
+    original_text: &'r str,
+    translated_text: &'r str,
 }
 
 #[get("/")]
-fn root() -> Json<Ok> {
-    let response: Ok = Ok {
-        message: "Hello, World!",
+fn root() -> Json<RootResponse<'static>> {
+    let response: RootResponse = RootResponse {
+        version: "0.1.0",
+        status: "Ok",
     };
     Json(response)
 }
 
-#[get("/add/<num1>/<num2>")]
-fn add(num1: &str, num2: &str) -> Json<Response> {
-    let n1: f64 = num1.parse().unwrap();
-    let n2: f64 = num1.parse().unwrap();
-    let response = Response {
-        params: format!("{} {}", &num1, &num2),
-        response: format!("{}", n1 + n2),
+#[get("/languages")]
+fn languages() -> Json<LanguageResponse<'static>> {
+    let response: LanguageResponse = LanguageResponse {
+        languages: ["fr", "en", "hi", "bn", "ka", "de", "af", "aa", "ab", "sq"],
+    };
+    Json(response)
+}
+
+#[post("/translate", data = "<body>")]
+fn translate(body: Json<TranslateRequest>) -> Json<TranslateResponse> {
+    let response: TranslateResponse = TranslateResponse {
+        success: true,
+        source_language: body.source_lang,
+        target_language: body.target_lang,
+        original_text: body.text,
+        translated_text: "<placeholder>",
     };
     Json(response)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![root, add])
+    rocket::build().mount("/", routes![root, languages, translate])
 }
